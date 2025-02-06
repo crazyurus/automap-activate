d3e2f6ddf2d6bfeb0613b02e0e22699e6d23316e
Cr4zy Uru5 <crazyurus@vip.qq.com>
2025-02-06 14:46:02 +0800

    chore: 更新依赖

--- api/app.ts
+++ api/app.ts
@@ -1,13 +1,8 @@
-import Koa, { Context } from 'koa';
+import Koa from 'koa';
 import body from 'koa-body';
 import logger from 'koa-logger';
 
 const app = new Koa();
-
-app.use(async (ctx: Context, next) => {
-  console.info(`access url: ${ctx.url}`);
-  await next();
-});
 
 app.use(body());
 app.use(logger());
