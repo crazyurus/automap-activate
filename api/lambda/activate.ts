e762e1491dfe3784fa0c7eba0d442bfc906d901d
Cr4zy Uru5 <crazyurus@vip.qq.com>
2025-08-08 18:22:32 +0800

    feat: 增加错误处理

--- api/lambda/activate.ts
+++ api/lambda/activate.ts
@@ -21,36 +21,44 @@ export async function post(
     };
   }
 
-  const response = await fetch(process.env.BYD_MAP_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      localNum: phone,
-    }),
-  });
-  const { code, success, message, result } = await response.json();
+  try {
+    const response = await fetch(process.env.BYD_MAP_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        localNum: phone,
+      }),
+    });
+    const { code, success, message, result } = await response.json();
 
-  if (success) {
-    if (result.vin === null || result.vin.slice(-6) === vin) {
-      return {
-        code: 0,
-        message: result.state,
-        data: {},
-      };
-    } else {
-      return {
-        code: -2,
-        message: '车架号后 6 位与本机号码不匹配',
-        data: null,
-      };
+    if (success) {
+      if (result.vin === null || result.vin.slice(-6) === vin) {
+        return {
+          code: 0,
+          message: result.state,
+          data: {},
+        };
+      } else {
+        return {
+          code: -2,
+          message: '车架号后 6 位与本机号码不匹配',
+          data: null,
+        };
+      }
     }
-  }
 
-  return {
-    code,
-    message,
-    data: null,
-  };
+    return {
+      code,
+      message,
+      data: null,
+    };
+  } catch (e) {
+    return {
+      code: -1,
+      message: '查询失败，请稍后再试',
+      data: null,
+    };
+  }
 }
